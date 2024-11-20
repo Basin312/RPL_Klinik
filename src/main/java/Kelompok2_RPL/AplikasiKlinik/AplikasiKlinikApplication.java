ec2a703eb736c848b200fa9a92bb7ee127edcfbd
Alexander Vinchent N <alexvinchent312.talenta@gmail.com>
2024-11-20 10:13:13 +0700

    gradle inisialization

---
+++ src/main/java/Kelompok2_RPL/AplikasiKlinik/AplikasiKlinikApplication.java
@@ -0,0 +1,13 @@
+package Kelompok2_RPL.AplikasiKlinik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiKlinikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiKlinikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Kelompok2_RPL/AplikasiKlinik/AplikasiKlinikApplicationTests.java
@@ -0,0 +1,13 @@
+package Kelompok2_RPL.AplikasiKlinik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiKlinikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
