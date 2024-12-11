d07b6156500650e85a314c8d7af92f085d8aedf7
Nikolas Giovanni <nikolas.giovanni1@gmail.com>
2024-12-11 23:40:15 +0700

    Fix: home dokter (filter sudah bekerja)

--- src/main/java/Kelompok2_RPL/AplikasiKlinik/pendaftaran/JdbcPendaftranKonsultasiRepository.java
+++ src/main/java/Kelompok2_RPL/AplikasiKlinik/pendaftaran/JdbcPendaftranKonsultasiRepository.java
@@ -9,7 +9,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class JdbcPendaftranKonsultasiRepository {
+public class JdbcPendaftranKonsultasiRepository implements PendaftaranKonsultasiRepository {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -20,7 +20,8 @@ public class JdbcPendaftranKonsultasiRepository {
         FROM Pendaftaran pd
         JOIN Pasien p ON pd.id_Pasien = p.id_Pasien
         JOIN Jadwal j ON pd.id_Jadwal = j.id_Jadwal
-        WHERE pd.tanggal_pendaftaran = CURRENT_DATE AND pd.is_Daftar = TRUE AND j.id_Dokter = ?
+        WHERE pd.tanggal_pendaftaran = CURRENT_DATE AND pd.is_Checkup = TRUE AND j.id_Dokter = ?
+        ORDER BY pd.nomor_antrian ASC
         """;
         
         List<PendaftaranKonsultasi> list = jdbcTemplate.query(sql, this::MapRowToPendaftaranKonsultasi, id_Dokter);
@@ -34,7 +35,8 @@ public class JdbcPendaftranKonsultasiRepository {
         FROM Pendaftaran pd
         JOIN Pasien p ON pd.id_Pasien = p.id_Pasien
         JOIN Jadwal j ON pd.id_Jadwal = j.id_Jadwal
-        WHERE pd.tanggal_pendaftaran = CURRENT_DATE AND pd.is_Daftar = TRUE AND j.id_Dokter = ? AND p.nama = ?
+        WHERE pd.tanggal_pendaftaran = CURRENT_DATE AND pd.is_Checkup = TRUE AND j.id_Dokter = ? AND p.nama ILIKE ?
+        ORDER BY pd.nomor_antrian ASC
         """;
 
         String keyword = "%" + filter + "%";
@@ -47,6 +49,9 @@ public class JdbcPendaftranKonsultasiRepository {
         boolean isDaftar = resultSet.getBoolean("is_Daftar");
         String statusDaftar = isDaftar ? "Menunggu" : "Belum daftar ulang";
 
+        boolean isCheckup = resultSet.getBoolean("is_Checkup");
+        String statusCheckup = isCheckup ? "Menunggu" : "Belum checkup";
+
         return new PendaftaranKonsultasi(
             resultSet.getInt("id_Pendaftaran"),
             resultSet.getDate("tanggal_pendaftaran"),
@@ -54,7 +59,8 @@ public class JdbcPendaftranKonsultasiRepository {
             resultSet.getInt("id_Jadwal"),
             resultSet.getInt("id_Pasien"),
             statusDaftar,
-            statusDaftar,
+            statusCheckup,
+            statusCheckup,
             resultSet.getString("nama")
         );
     }
